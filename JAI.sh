538242e0eb7bfd1e0cbb5363c57005bce918e8b5
MrK15UD288 <56219005+MrK15UD288@users.noreply.github.com>
2019-10-08 20:49:41 +0700

    Add files via upload

---
+++ JAI.sh
@@ -0,0 +1,376 @@
+clear
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+echo $bi          "         .﹎ ┈ ┈ ┈ ﹎"
+echo $pur         "         ﹎┈  ●  o  .﹎ ﹎"  $cy "V.1"
+echo $pur         "         ┈ ┈ /█\/▓\ ﹎ ┈"
+echo $pur         "          ▅▆▇█████▇▆▅"
+echo $cy          "       CP : 0858 7655 0051"
+echo
+echo
+echo
+sleep 1
+echo $cy "╔═════════════════════•<~~~>•═════════════════════╗"
+echo $ku "        Gapailah ambisi Anda setinggi langit!"
+sleep 1
+echo $pu "      Mimpikan ambisi Anda itu setinggi langit!"
+sleep 1
+echo $ku "              Karena jika Anda jatuh,"
+sleep 1
+echo $pu "     Anda akan jatuh di antara bintang-bintang!!"
+sleep 1
+echo $bi "                     Ir SOEKARNO"
+echo $cy "╚═════════════════════•<~~~>•═════════════════════╝"
+echo
+
+echo
+echo
+echo $me"Masukkan Username"
+read -p " ╰┈─➤   " user
+sleep 2
+echo $i"Masukkan Password"
+read -p " ╰┈─➤  " pass
+
+if [ $user = 'KELENG' ]|[ $pass = 'KESUGIHAN' ]
+then
+echo
+echo
+echo
+echo
+echo $pur      "╔╗   ╔═══╗╔═══╗╔══╗╔═╗ ╔╗"
+echo $pur      "║║   ║╔═╗║║╔═╗║╚╣─╝║║╚╗║║"
+echo $pur      "║║   ║║ ║║║║ ╚╝ ║║ ║╔╗╚╝║"
+echo $pur      "║║ ╔╗║║ ║║║║╔═╗ ║║ ║║╚╗║║"
+echo $pur      "║╚═╝║║╚═╝║║╚╩ ║╔╣─╗║║ ║║║"
+echo $pur      "╚═══╝╚═══╝╚═══╝╚══╝╚╝ ╚═╝"
+
+echo $i "╔═══╗╔╗ ╔╗╔╗╔═╗╔═══╗╔═══╗╔═══╗"
+echo $i "║╔═╗║║║ ║║║║║╔╝║╔═╗║║╔══╝║╔═╗║"
+echo $i "║╚══╗║║ ║║║╚╝╝ ║╚══╗║╚══╗║╚══╗"
+echo $i "╚══╗║║║ ║║║╔╗║ ╚══╗║║╔══╝╚══╗║"
+echo $i "║╚═╝║║╚═╝║║║║╚╗║╚═╝║║╚══╗║╚═╝║"
+echo $i "╚═══╝╚═══╝╚╝╚═╝╚═══╝╚═══╝╚═══╝"
+sleep 2
+else
+
+echo $me "USER & PASS ~> SALAH TOLOL!"
+echo $cy "Silahkan hubungi MrK15UD"
+exit
+fi
+clear
+echo $bi "╔╗─╔═╗╔══╗╔══╗╔══╗╔═╦╗╔══╗"
+echo $bi "║║─║║║║╔╗║╚╗╗║╚║║╝║║║║║╔═╣"
+echo $bi "║╚╗║║║║╠╣║╔╩╝║╔║║╗║║║║║╚╗║"
+echo $bi "╚═╝╚═╝╚╝╚╝╚══╝╚══╝╚╩═╝╚══╝"
+sleep 2
+echo $pu "▓▓▓▓▓▓▓" $me "10%"
+sleep 1
+echo $bi "╔╗─╔═╗╔══╗╔══╗╔══╗╔═╦╗╔══╗"
+echo $bi "║║─║║║║╔╗║╚╗╗║╚║║╝║║║║║╔═╣"
+echo $bi "║╚╗║║║║╠╣║╔╩╝║╔║║╗║║║║║╚╗║"
+echo $bi "╚═╝╚═╝╚╝╚╝╚══╝╚══╝╚╩═╝╚══╝"
+sleep 2
+echo $pu "▓▓▓▓▓▓▓▓▓▓▓" $me "30%"
+sleep 1
+echo $bi "╔╗─╔═╗╔══╗╔══╗╔══╗╔═╦╗╔══╗"
+echo $bi "║║─║║║║╔╗║╚╗╗║╚║║╝║║║║║╔═╣"
+echo $bi "║╚╗║║║║╠╣║╔╩╝║╔║║╗║║║║║╚╗║"
+echo $bi "╚═╝╚═╝╚╝╚╝╚══╝╚══╝╚╩═╝╚══╝"
+sleep 2
+echo $pu "▓▓▓▓▓▓▓▓▓▓▓▓▓▓"  $me "50%"
+sleep 2
+echo $bi "╔╗─╔═╗╔══╗╔══╗╔══╗╔═╦╗╔══╗"
+echo $bi "║║─║║║║╔╗║╚╗╗║╚║║╝║║║║║╔═╣"
+echo $bi "║╚╗║║║║╠╣║╔╩╝║╔║║╗║║║║║╚╗║"
+echo $bi "╚═╝╚═╝╚╝╚╝╚══╝╚══╝╚╩═╝╚══╝"
+sleep 2
+echo $pu "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" $me "80%"
+echo $bi "╔╗─╔═╗╔══╗╔══╗╔══╗╔═╦╗╔══╗"
+echo $bi "║║─║║║║╔╗║╚╗╗║╚║║╝║║║║║╔═╣"
+echo $bi "║╚╗║║║║╠╣║╔╩╝║╔║║╗║║║║║╚╗║"
+echo $bi "╚═╝╚═╝╚╝╚╝╚══╝╚══╝╚╩═╝╚══╝"
+sleep 3
+echo $pu "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" $me "100%"
+clear
+
+echo $me "  ╗─╔╗╔═══╗╔╗───╔╗───╔═══╗"
+echo $me " ║║─║║║╔══╝║║───║║───║╔═╗║"
+echo $me " ║╚═╝║║╚══╗║║───║║───║║─║║"
+echo $pu " ║╔═╗║║╔══╝║║─╔╗║║─╔╗║║─║║"
+echo $pu " ║║─║║║╚══╗║╚═╝║║╚═╝║║╚═╝║"
+echo $pu " ╚╝─╚╝╚═══╝╚═══╝╚═══╝╚═══╝ V.1 "
+sleep 2
+echo
+echo
+echo $ku "╔═══════════════•ೋೋ•══════════════╗"
+echo $bi " Author  || MrK15UD"
+echo $bi " Team    || 🌹 Java Attacker Indo🌹"
+echo $bu "  CP      || 0858 7655 0051"
+echo $ku "╚═══════════════•ೋೋ•══════════════╝"
+echo $i  "   V.1||2019||MrK15UD||INDONESIA"
+echo
+sleep 1
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  1.Spam SMS"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  2.Phising Game"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+sleep 2
+echo $ku "  3.Script Deface"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  4.Hack FB VIP"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  5.Perusak HP"
+echo $me "╚══════•ೋೋ•═══════╝"
+sleep 2
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  6.Garuda DDoS"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  7.Red Hawk"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  8.Locator"
+echo $me "╚══════•ೋೋ•═══════╝"
+sleep 2
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  9.Userrecon"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  10.Install Lazymux"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  11.Tombol Special"
+echo $me "╚══════•ೋೋ•═══════╝"
+sleep 2
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  12.Hack Camera HP"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  13.Install Weeman"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  14.Fake Call"
+echo $me "╚══════•ೋೋ•═══════╝"
+sleep 2
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  15.Penangkal Virus"
+echo $me "╚══════•ೋೋ•═══════╝"
+echo $me "╔══════•ೋೋ•═══════╗"
+echo $ku "  16.DDoS rus"
+echo $me "╚══════•ೋೋ•═══════╝"
+sleep 1
+echo $cy "[====Pilih Tools====]"
+echo    "╭─MrK15UD" |lolcat
+read -p "╰─>" pil;
+
+case $pil in
+
+#spam sms
+
+1)git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+
+
+;;
+
+#phising game
+
+2)git clone https://github.com/CyberTCA/Phising-Game
+cd Phising-Game
+sh phising.sh
+
+
+;;
+
+#script deface
+
+3)git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+
+
+;;
+
+#hack FB VIP
+
+4)clear
+pkg install python2
+git clone https://github.com/CyberTCA/FbVIP
+cd FbVIP
+python2 FbVIP.py
+
+;;
+
+#perusak HP
+
+5)pkg install python git
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+;;
+
+#Deface webdav74
+
+6)clear
+pkg install python git
+git clone https://github.com/Bl4ckDr460n/Garuda-DDOS.git
+cd Garuda-DDOS
+python garuda.py
+
+;;
+
+#Gerate KK/KTP
+
+7)clear
+ apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+ls
+cd RED_HAWK
+ls
+php rhawk.php
+
+
+;;
+
+#Lacak Orang
+
+8)pkg install php
+pkg install curl
+pkg install openssh
+pkg install python python2 -y
+git clone https://github.com/thelinuxchoice/locator
+ls
+cd locator
+ls
+bash locator.sh
+
+;;
+
+#userrecon
+
+9)git clone https://github.com/thelinuxchoice/userrecon
+clear
+ls
+cd userrecon
+ls
+bash userrecon.sh
+
+;;
+
+#lazymux
+
+10)clear
+apt install python2
+git clone https://github.com/Gameye98/Lazymux.git
+cd Lazymux
+python2 lazymux.py
+
+;;
+
+#Tombol Special
+
+11)clear
+pkg install php
+git clone https://github.com/MiSetya/tombol-special
+cd tombol-special
+php Aktif.php
+
+;;
+
+#Hack Camera HP
+
+12)clear
+pkg install fish
+git clone  https://github.com/TheSploit/Hack-Cam_Phone
+pkg install openssh
+cd Hack-Cam_Phone
+clear
+bash saycheese.sh
+
+;;
+
+# install weeman
+
+13)clear
+pkg install python2
+pip2 install beautifulsoup
+pkg install git
+git clone https://github.com/evait-security/weeman
+cd weeman
+clear
+python2 weeman.py
+
+;;
+
+#Fake Call
+
+14)clear
+pkg install toilet
+pkg install openssh
+pkg install php
+pkg install nodejs
+pkg install nodejs-lts
+pkg install python python2
+pkg install python2-dev
+pkg install git
+pkg install curl
+pkg install figlet
+pkg install ruby
+gem install lolcat
+git clone https://github.com/T14RB373T/FAKECALL
+cd FAKECALL
+clear
+bash call.sh
+
+;;
+
+#penangkal virus
+
+15)clear
+pkg install python2
+git clone https://github.com/JustAHackers/penangkal
+bash setup
+
+
+;;
+
+#DDoS Rus
+
+16)clear
+pkg install git
+pkg install bash
+pkg install python
+pip install requests
+git clone https://github.com/Rusmana-ID/ddos-website
+cd ddos-website
+bash ddos.sh
+
+
+;;
+
+*) echo $i "THANKS FOR USING,SUBSCRIBE LAH"
+exit
+esac
